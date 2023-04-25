4bc3ab5b86517f657b52be0784209c101d31c718
GalaxyUniv <100881157+GalaxyUniv@users.noreply.github.com>
2023-04-25 19:17:13 -0300

    Add files via upload

---
+++ screens/PostScreen.js
@@ -0,0 +1,349 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Speech from "expo-speech";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import firebase from "firebase";
+
+export default class StoryScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      speakerColor: "gray",
+      speakerIcon: "volume-high-outline",
+      light_theme: false,
+      //  likes: this.props.route.params.story.likes,
+      is_liked: false,
+    };
+  }
+  /*likeAction = () => {
+    if (this.state.is_liked) {
+      firebase
+        .database()
+        .ref("posts")
+        .child(this.props.route.params.story_id)
+        .child("likes")
+        .set(firebase.database.ServerValue.increment(-1));
+      this.setState({ likes: (this.state.likes -= 1), is_liked: false });
+    } else {
+      firebase
+        .database()
+        .ref("posts")
+        .child(this.props.route.params.story_id)
+        .child("likes")
+        .set(firebase.database.ServerValue.increment(1));
+      this.setState({ likes: (this.state.likes += 1), is_liked: true });
+    }
+  };*/
+
+  /*componentDidMount() {
+    
+  }*/
+
+  async fetchUser() {
+    let theme, name;
+    await firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", function (data) {
+        theme = data.val().current_theme;
+        name = `${data.val().first_name} ${data.val().last_name} `;
+      });
+    this.setState({
+      light_theme: theme === "light" ? true : false,
+      isEnabled: theme === "light" ? false : true,
+      name: name,
+    });
+  }
+
+  async speaker(title, author, story, moral) {
+    const currentColor = this.state.speakerColor;
+    this.setState({
+      speakerColor: currentColor === "gray" ? "#ee8249" : "gray",
+    });
+    if (currentColor === "gray") {
+      Speech.speak(title);
+      Speech.speak("Escrito Por" + author);
+      Speech.speak(story);
+      Speech.speak("Moral da História:" + moral);
+    } else {
+      Speech.stop();
+    }
+  }
+
+  render() {
+    if (!this.props.route.params) {
+      this.props.navigation.navigate("Home");
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text
+                style={
+                  this.state.light_theme
+                    ? styles.appTitleTextLight
+                    : styles.appTitleText
+                }
+              >
+                Espectagram
+              </Text>
+            </View>
+          </View>
+          <View style={styles.storyContainer}>
+            <ScrollView
+              style={
+                this.state.light_theme
+                  ? styles.storyCardLight
+                  : styles.storyCard
+              }
+            >
+              <Image
+                source={require("../assets/image_1.jpg")}
+                style={styles.image}
+              ></Image>
+
+              <View style={styles.dataContainer}>
+                <View style={styles.titleTextContainer}>
+                  <Text
+                    style={
+                      this.state.light_theme
+                        ? styles.storyTitleTextLight
+                        : styles.storyTitleText
+                    }
+                  >
+                    {this.props.route.params.post.title}
+                  </Text>
+                  <Text
+                    style={
+                      this.state.light_theme
+                        ? styles.storyAuthorTextLight
+                        : styles.storyAuthorText
+                    }
+                  >
+                    {this.props.route.params.post.author}
+                  </Text>
+                  <Text
+                    style={
+                      this.state.light_theme
+                        ? styles.storyAuthorTextLight
+                        : styles.storyAuthorText
+                    }
+                  >
+                    {this.props.route.params.post.created_on}
+                  </Text>
+                </View>
+                {this.props.route.params.post.author}
+                <TouchableOpacity
+                  style={styles.iconContainer}
+                  onPress={() => {
+                    this.speaker(
+                      this.props.route.params.post.title,
+                      this.props.route.params.post.author,
+                      this.props.route.params.post.story,
+                      this.props.route.params.post.moral
+                    );
+                  }}
+                >
+                  <Ionicons
+                    name={this.state.speakerIcon}
+                    size={RFValue(30)}
+                    color={this.state.speakerColor}
+                    style={{ margin: RFValue(15) }}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.storyTextContainer}>
+                <Text
+                  style={
+                    this.state.light_theme
+                      ? styles.storyTextLight
+                      : styles.storyText
+                  }
+                >
+                  {this.props.route.params.post.post}
+                </Text>
+                <Text
+                  style={
+                    this.state.light_theme
+                      ? styles.moralTextLight
+                      : styles.moralText
+                  }
+                >
+                  Moral - {this.props.route.params.post.moral}
+                </Text>
+              </View>
+              <View style={styles.actionContainer}>
+                <TouchableOpacity style={styles.likeButton}>
+                  <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                  <Text style={styles.likeText}>{this.state.likes}</Text>
+                </TouchableOpacity>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000066",
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: "#FFFAF0",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  appTitleTextLight: {
+    color: "black",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  storyContainer: {
+    flex: 1,
+  },
+  storyCard: {
+    margin: RFValue(20),
+    backgroundColor: "#666699",
+    borderRadius: RFValue(20),
+  },
+  storyCardLight: {
+    margin: RFValue(20),
+    backgroundColor: "#FFEBCD",
+    borderRadius: RFValue(20),
+  },
+  image: {
+    width: "100%",
+    alignSelf: "center",
+    height: RFValue(200),
+    borderTopLeftRadius: RFValue(20),
+    borderTopRightRadius: RFValue(20),
+    resizeMode: "contain",
+  },
+  dataContainer: {
+    flexDirection: "row",
+    padding: RFValue(20),
+  },
+  titleTextContainer: {
+    flex: 0.8,
+  },
+  storyTitleText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    color: "white",
+  },
+  storyTitleTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    color: "black",
+  },
+  storyAuthorText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(18),
+    color: "white",
+  },
+  storyAuthorTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(18),
+    color: "Black",
+  },
+  iconContainer: {
+    flex: 0.2,
+  },
+  storyTextContainer: {
+    padding: RFValue(20),
+  },
+  storyText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(15),
+    color: "white",
+  },
+  storyTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(15),
+    color: "black",
+  },
+  moralText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(20),
+    color: "white",
+  },
+  moralTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(20),
+    color: "black",
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: RFValue(10),
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: RFValue(30),
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5),
+  },
+  likeTextLight: {
+    color: "black",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5),
+  },
+});
